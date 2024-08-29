7bb602f1470fb2d262dd7d7cf7d83794bf0c8f0e
Michael Maley <153663136+OneSolarMikey@users.noreply.github.com>
2024-08-29 13:00:06 -0400

    01_03_infinite_skills.py

--- 01_getting_started/01_03_infinite_skills.py
+++ 01_getting_started/01_03_infinite_skills.py
@@ -6,4 +6,6 @@
 
 2) I've always wanted to make a game from code as well so if I had that power right now I would do so.
 
-3) If I also had the power of coding right now I would definetly want to help people and teach them the power as well.'''
+3) If I also had the power of coding right now I would definetly want to help people and teach them the power as well.
+
+'''
